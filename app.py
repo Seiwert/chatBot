7a1d5d6de069d220288ccc5d3248a5f7c68558e0
Seiwert <adamseiwert@gmail.com>
2016-12-12 00:48:49 -0600

    reworked directory. Now talking with Alexa Skills Service

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, jsonify
+from flask_ask import Ask, statement
+
+app = Flask(__name__)
+ask = Ask(app,'/')
+
+@ask.launch
+def launch():
+    text = execute.decode_line(sess, model, enc_vocab, rev_dec_vocab, 'Hi' )
+    return statement(text).simple_card(text)
+
+@ask.intent('TalkWithMeIntent')
+def response():
+    text = execute.decode_line(sess, model, enc_vocab, rev_dec_vocab, 'this is a response' )
+    return statement(text).simple_card(text)
+
+#_________________________________________________________________
+import tensorflow as tf
+import execute
+
+sess = tf.Session()
+sess, model, enc_vocab, rev_dec_vocab = execute.init_session(sess, conf='seq2seq_serve.ini')
+#_________________________________________________________________
+
+@ask.intent('AMAZON.CancelIntent')
+def cancel():
+    return statement("Goodbye")
+
+@ask.intent('AMAZON.StopIntent')
+def stop():
+    return statement("Goodbye")
+
+@ask.session_ended
+def session_ended():
+    return "", 200
+
+# start app
+if (__name__ == "__main__"): 
+    app.run(port = 5000) 
